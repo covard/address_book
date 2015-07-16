3def7918b2cb49ea1feb15a8149b927636656c50
Curtis Ovard <ovardcj@gmail.com>
2015-07-15 20:20:58 -0600

    add validators for address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -10,6 +10,9 @@ class Address
   field :first_name, type: String
   field :last_name, type: String
 
+  validates :first_name, :last_name, :city, :state, :zip, :phone, :address_1, presence: true
+  validates_format_of :email, with: /.+@.+\..+/i
+
   def full_name
     "#{first_name} #{last_name}"
   end
